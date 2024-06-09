33e9de6037f3802bb87b58f5901e5486b34b0367
Sibel Bouzo <bouzo.sibel@gmail.com>
2024-06-09 12:44:22 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cv-it/",
   plugins: [react()],
   resolve: {
     alias: {
