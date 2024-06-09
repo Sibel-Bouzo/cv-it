0524f9da0d9a9278503442465498d5d206b98b1e
Sibel Bouzo <bouzo.sibel@gmail.com>
2024-06-09 16:17:53 -0700

    style changes

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className="bg-[#191919] pt-16 ">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-14 w-[80%] m-auto mb-10">
-        <img src="images/cv1.png" className="w-28 " />
+        <img src="images/logo.png" className="w-28 " />
         <div>
           <h3 className="mb-4 font-semibold text-gray-600 text-2xl">Explore</h3>
           <ul>
@@ -25,7 +25,8 @@ export const Footer = () => {
           <ul className="flex !list-none">
             <li className="mr-4">
               <a
-                href=""
+                href="https://www.linkedin.com/in/sibel-bouzo/"
+                target="_blank"
                 className="bg-[#313131] text-2xl w-11 h-11 rounded-[50%] text-[#f5f5f5] inline-flex justify-center items-center duration-300 hover:bg-[#0077B5]"
               >
                 <FaLinkedinIn />
@@ -33,7 +34,8 @@ export const Footer = () => {
             </li>
             <li className="mr-4">
               <a
-                href=""
+                href="https://github.com/Sibel-Bouzo"
+                target="_blank"
                 className="bg-[#313131] w-11 h-11 rounded-[50%] text-2xl text-[#f5f5f5] inline-flex justify-center items-center duration-300 hover:bg-white hover:text-[#313131]"
               >
                 <BsGithub />
--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -4,7 +4,11 @@ export const Nav = () => {
   return (
     <header className="bg-white shadow-sm w-full header m-auto px-16">
       <nav className="flex flex-col sm:flex-row justify-between items-center relative">
-        <img src="images/cv1.png" alt="logo" className="w-[20%] sm:w-20" />
+        <img
+          src="images/logo.png"
+          alt="logo"
+          className="w-[20%] sm:w-[4.6rem]"
+        />
         <ul className="flex !list-none m-auto sm:m-0 main-nav">
           {navEl.map((item, index) => (
             <li key={index}>
