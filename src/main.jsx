5f316c7eee9c9643278732b5716af6ca41199620
Sibel Bouzo <bouzo.sibel@gmail.com>
2024-06-12 16:41:43 -0700

    Hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AppProvider } from "./contexts/Context.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppProvider>
       <App />
     </AppProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
